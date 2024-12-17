f1aa05bd6bd04ab9a171bb749573772306928981
JairoDH <jairodominguez3594@gmail.com>
2024-12-17 02:19:00 +0100

    .

--- scriptbackup.sh
+++ scriptbackup.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+!/bin/bash
 
+# Configuraciones de la base de datos
 WORDPRESS_DB_USER="wpuser"
 WORDPRESS_DB_PASSWORD="wp1994"
 WORDPRESS_DB_NAME="wordpress"
-#MYSQL_DB_HOST=10.43.74.12
+
+# Obtener el host de MySQL desde Kubernetes
 MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
 
-mysqldump --set-gtid-purged=OFF --no-tablespaces -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
+# Realizar el volcado de la base de datos
+mysqldump --no-column-statistics --set-gtid-purged=OFF --no-tablespaces -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
+
+# Verificar si el volcado fue exitoso
+if [ $? -eq 0 ]; then
+    echo "Respaldo de la base de datos completado con éxito."
+else
+    echo "Error en el respaldo de la base de datos."
+    exit 1
+fi
+
