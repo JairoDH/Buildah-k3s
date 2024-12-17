7afd37fde056b09e625b1f78c5dc8c3f33ba777c
JairoDH <jairodominguez3594@gmail.com>
2024-12-17 02:04:52 +0100

    .

--- scriptbackup.sh
+++ scriptbackup.sh
@@ -6,4 +6,4 @@ WORDPRESS_DB_NAME="wordpress"
 #MYSQL_DB_HOST=10.43.74.12
 MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
 
-mysqldump --column-statistics=0 -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
+mysqldump --set-gtid-purged=OFF --no-tablespaces -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
