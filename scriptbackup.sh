99a8506e491efc15070408b7c8bb36d972c60f6d
JairoDH <jairodominguez3594@gmail.com>
2024-12-17 02:37:22 +0100

    .

--- scriptbackup.sh
+++ scriptbackup.sh
@@ -3,7 +3,8 @@
 WORDPRESS_DB_USER="wpuser"
 WORDPRESS_DB_PASSWORD="wp1994"
 WORDPRESS_DB_NAME="wordpress"
-MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
+MYSQL_DB_HOST=10.43.74.12
+#MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
 
 # Realizar el volcado de la base de datos sin la opción '--no-column-statistics'
 mysqldump --set-gtid-purged=OFF --no-tablespaces -u $WORDPRESS_DB_USER -p"$WORDPRESS_DB_PASSWORD" -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
