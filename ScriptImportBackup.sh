db38995ef96e15c0565e13619da03189f3efa450
JairoDH <jairodominguez3594@gmail.com>
2024-12-17 01:45:49 +0100

    scripts de Exportación y Importación

---
+++ ScriptImportBackup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+WORDPRESS_DB_USER="wpuser"
+WORDPRESS_DB_PASSWORD="wp1994"
+WORDPRESS_DB_NAME="wordpress"
+#MYSQL_DB_HOST=10.43.74.12
+MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
+
+mysql -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME < /home/jairo/databd.sql
