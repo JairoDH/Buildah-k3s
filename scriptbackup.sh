5f5b301e53180d266f541c18839ea07786f14ab3
JairoDH <jairodominguez3594@gmail.com>
2024-12-18 12:56:37 +0100

    cambiando IP del contenedor mysql para la exportacion de la bd

--- scriptbackup.sh
+++ scriptbackup.sh
@@ -3,19 +3,9 @@
 WORDPRESS_DB_USER="wpuser"
 WORDPRESS_DB_PASSWORD="wp1994"
 WORDPRESS_DB_NAME="wordpress"
-MYSQL_DB_HOST=10.43.74.12
+MYSQL_DB_HOST=10.43.242.80
 #MYSQL_DB_HOST=$(kubectl get svc mysql -o jsonpath='{.spec.clusterIP}')
 
-# Realizar el volcado de la base de datos sin la opción '--no-column-statistics'
-mariadb-dump -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
-
-# Verificar si el volcado fue exitoso
-if [ $? -eq 0 ]; then
-    echo "Respaldo de la base de datos completado con éxito."
-else
-    echo "Error en el respaldo de la base de datos."
-    exit 1
-fi
 # Realizar el volcado de la base de datos sin la opción '--no-column-statistics'
 mariadb-dump -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
 
