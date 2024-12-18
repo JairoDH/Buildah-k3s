fb21041e2ab77f9beb923867c8618bafcf0b9bf8
JairoDH <jairodominguez3594@gmail.com>
2024-12-18 12:33:08 +0100

    dev.touristmap.es

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -86,8 +86,8 @@ $table_prefix = 'wp_';
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
 define( 'WP_DEBUG', false );
-define('WP_HOME', 'http://www.touristmap.es');
-define('WP_SITEURL', 'http://www.touristmap.es');
+define('WP_HOME', 'http://dev.touristmap.es');
+define('WP_SITEURL', 'http://dev.touristmap.es');
 
 /* Add any custom values between this line and the "stop editing" line. */
 
