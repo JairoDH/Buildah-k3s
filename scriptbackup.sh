c6a6c49cdd1b8f9df52bedd7e80f7df205a3ebee
JairoDH <jairodominguez3594@gmail.com>
2024-12-19 00:12:48 +0100

    .

--- scriptbackup.sh
+++ scriptbackup.sh
@@ -16,13 +16,3 @@ else
     echo "Error en el respaldo de la base de datos."
     exit 1
 fi
-# Realizar el volcado de la base de datos sin la opción '--no-column-statistics'
-mariadb-dump -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $MYSQL_DB_HOST $WORDPRESS_DB_NAME > /home/jairo/databd.sql
-
-# Verificar si el volcado fue exitoso
-if [ $? -eq 0 ]; then
-    echo "Respaldo de la base de datos completado con éxito."
-else
-    echo "Error en el respaldo de la base de datos."
-    exit 1
-fi
